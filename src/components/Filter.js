1040b3c43759899a55f6d0d3bf7d17e28c9a0b49
github username <github email address>
2018-01-15 19:47:11 -0500

    Done.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react';
 
-class Filter extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      filters: []
-    };
-  }
-
-  componentWillMount() {
-    this.fetchFilters();
-  }
-
-  fetchFilters = () => {
-    fetch('/api/fruit_types')
-      .then(response => response.json())
-      .then(filters => this.setState({ filters }));
-  }
-
-  render() {
+const Filter = (props) => {
     return (
-      <select onChange={this.props.handleChange} defaultValue='all'>
+      <select onChange={props.handleChange} >
         <option value='all'>All</option>
-        {this.state.filters.map(filter =>
+        {props.filters.map(filter =>
           <option key={filter} value={filter}>{filter}</option>
         )}
       </select>
     );
   }
-}
 
+
+  Filter.defaultProps = {
+    filters: [],
+    handleChange: null
+  }
 export default Filter;
